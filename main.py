ad2e5c76fc64992ecf0bb8a66035e1e8bbde7044
Luto228 <megapupsik909@gmail.com>
2026-04-28 14:04:38 +0400

    You can write complete sentences without quotation marks

--- main.py
+++ main.py
@@ -4,23 +4,15 @@ import sys
 import os
 
 def main():
-    if len(sys.argv) == 2:
-        command = sys.argv[1]
-
-        try:
-            webbrowser.open(f"https://translate.google.com/?sl=auto&tl=en&text={command}&op=translate")
-        except webbrowser.Error as e:
-            raise ValueError(f'Failed to open web browser: {e}')
-    elif len(sys.argv) == 3:
-        command = sys.argv[1]
-        language = sys.argv[2]
-
-        try:
-            webbrowser.open(f"https://translate.google.com/?sl=auto&tl={language}&text={command}&op=translate")
-        except webbrowser.Error as e:
-            raise ValueError(f'Failed to open web browser: {e}')
-    else:
-        raise ValueError('Please write trans [text] or trans [text] [language]')
-    
+    if len(sys.argv) >= 2:
+        command = sys.argv[1:]
+        last_word = command[-1].lower()
+        last_word_length = len(last_word)
+        if last_word_length == 2 and len(command) > 1:
+            command_string = " ".join(command[:-1])
+            webbrowser.open(f"https://translate.google.com/?sl=auto&tl={last_word}&text={command_string}&op=translate")
+        else:
+            command_string = " ".join(command)
+            webbrowser.open(f"https://translate.google.com/?sl=auto&tl=en&text={command_string}&op=translate")
 if __name__ == "__main__":
     main()
